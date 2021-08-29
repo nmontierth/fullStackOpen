46b40303d9a526fc7434f7bfb8271972b2631037
Noah Montierth <nmontierth7@yahoo.com>
2021-08-28 23:37:49 -0400

    v1

--- pt1/task1/src/App.js
+++ pt1/task1/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Header = (props) => {
+  return(
+  <>
+    <h1>{props.course}</h1>
+  </>
+  )
+}
+
+const Part = (props) => {
+  let part = props.parts[props.number]
+  let exercise = props.exercises[props.number]
+  return(
+    <>
+      <p>{part} {exercise}</p>
+    </>
+  )
+  }
+const Content = (props) => {
+  let parts = props.parts
+  let exercises = props.exercises
+  return(
+  <div>
+    <Part parts={parts} exercises={exercises} number = {0}/>
+    <Part parts={parts} exercises={exercises} number = {1}/>
+    <Part parts={parts} exercises={exercises} number = {2}/>
+  </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+  <>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  </>
+  )
+}
+
+
+
+
+
+const App = () => {
+  const course = 'Half Stack application development';
+  let parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+  let exercises = [10,7,14]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course}/>
+      <Content parts={parts} exercises={exercises}/>
+      <Total exercises1={exercises[0]} exercises2={exercises[1]} exercises3={exercises[2]}/>
     </div>
   );
 }
